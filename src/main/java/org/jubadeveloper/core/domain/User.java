67ca1d49175fab8d69859107e47f5379b54ba881
JubaDeveloper <michaelres1332@gmail.com>
2023-11-05 21:08:09 -0300

    Created User entitie

---
+++ src/main/java/org/jubadeveloper/core/domain/User.java
@@ -0,0 +1,53 @@
+package org.jubadeveloper.core.domain;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.time.LocalDate;
+
+@Entity()
+@Table(name = "'User'")
+public class User {
+    @Id
+    String email;
+    @Column(nullable = false)
+    String username = "";
+    @ColumnDefault("sysdate")
+    @Column(nullable = false, updatable = false)
+    LocalDate createdAt;
+    public User () {}
+    public User (String email, String username, LocalDate createdAt) {
+        this.email = email;
+        this.username = username;
+        this.createdAt = createdAt;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+}
